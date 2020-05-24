3eeeb1aabd478fbc54b3b3ea490c5aa1a3083b12
Aaron Horwin <61487056+horwina@users.noreply.github.com>
2020-05-23 21:22:33 -0400

    Update find_optimized_portfolio.py

--- find_optimized_portfolio.py
+++ find_optimized_portfolio.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 This program finds the efficient frontier according to Markowitz mean return-variance theory.
-The portfolio is then backtesed by testing it on random dates, and evaluating its performance
+The portfolio can then be backtested in two ways: applying the model to other periods
+and conducting Monte Carlo simulation
 
 Call main to begin program
 """
@@ -11,99 +12,96 @@ from DF_helper import *
 from prints import  *
 from plots import *
 from user import *
+from RealTest import realtest_frontier
+from MonteCarlo import MCStockSimulator
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+#### S&P performance from 2001-1-1 to 2020-5-1:
+#### avg. daily return: .0288% ; standard dev. of daily returns: 1.255%
+
 
 def main():
     """
     Begin program
     """
+    # initial ui
     start1,end1,tickers = get_portfolio_parameters()
     
-    optimal_portfolios = find_optimal_portfolios(start1,end1,tickers)
+    # returns data needed for analysis
+    stds,mean_returns,covar=process_data(start1,end1,tickers)
     
-    # Ask user whether to backtest or restart
-    next_step = backtest_or_restart()
-    if next_step =='backtest':
-        backtest(start1,end1,tickers)
-    elif next_step == 'restart':
-        main()
+    # find allocation weights and return of the minimum variance portfolio
+    w_glob_min,r_glob_min = find_global_min(stds,mean_returns,covar)
     
-def find_optimal_portfolios(start1,end1,tickers):
+    # print info about avg stats for tickers & min variance portfolio given those tickers
+    print_glob_min_variance(w_glob_min,r_glob_min,tickers,mean_returns,covar,stds,start1,end1)
+    
+    # get the standard deviation, returns, and allocation weights of the minimum
+    # variance portfolio for each return in rs
+    range_rs=.001
+    quantity_rs=40
+    rs = np.linspace(r_glob_min-range_rs,r_glob_min+range_rs,quantity_rs)
+    efficient_stds,efficient_weights = efficient_frontier(mean_returns,covar,rs)
+    
+    # plot frontier
+    plot_efficient_frontier(efficient_stds,rs)
+    
+    # choose what to do next
+    sequence(stds,mean_returns,covar,start1,end1,tickers)
+
+    
+    
+def process_data(start1,end1,tickers):
     """
-    Input start,end,tickers, and a list of desired returns
-    Returns the weights corresponding to the
-    minimum variance portfolio, in matrix format
+    Input start,end dates and tickers
+    Returns standard dev, mean_returns, and covar
     """
     prices=get_prices(start1,end1,tickers)
     returns=get_returns(prices)
     stds=returns.std()
     mean_returns = np_take_mean_of_returns(returns)
     covar=covar_to_np(returns)
+    return stds,mean_returns,covar
+    
 
+def find_global_min(stds,mean_returns,covar):
+    """
+    Input start,end,tickers, and a list of desired returns
+    Returns the weights corresponding to the
+    minimum variance portfolio, in matrix  format
+    """
+    
     #get array of weights for each ticker of min var portfolio
     w_glob_min = calc_global_min_variance_portfolio(covar)
     #dot weights with returns to get array of min var portfolio
     r_glob_min=np_calc_portfolio_return(mean_returns,w_glob_min)
     
-    #print 
-    print_glob_min_variance(w_glob_min,r_glob_min,tickers,mean_returns,covar,stds,start1,end1)
+    return w_glob_min,r_glob_min
 
-    # define a list of returns to find minimumn stdev for
-    range_rs=.001
-    quantity_rs=40
-    rs = np.linspace(r_glob_min-range_rs,r_glob_min+range_rs,quantity_rs)
-    
-    # get the standard deviation, returns, and allocation weights of the minimum
-    # variance portfolio for each return in rs
-    sigmas,weights,returns = calc_efficient_portfolios(mean_returns,covar,rs)
-    
-    plot_efficient_frontier(sigmas,returns)
-    
-    
-    
-def backtest(start1,end1,tickers):
+
+def sequence(stds,mean_returns,covar,start1,end1,tickers):
     """
-    Takes in 2d array of weights of all the calculated efficient portfolios,
-    applies them to a set of different dates for purpose of testing predictability
+    Ask user whether to backtest or restart
     """
-    # interact with user to choose a predictability test region (dates) and a return value (for purpose of choosing allocations)
-    desired_return,start2,end2=backtesting_details(start1,end1)
-    
-    prices=get_prices(start1,end1,tickers)
-    returns=get_returns(prices)
-    covar=covar_to_np(returns)
-    mean_returns=np_take_mean_of_returns(returns)
-    
-    st,weights,rett = calc_efficient_portfolios(mean_returns,covar,[desired_return])
-    
-    # Get returns for second time period
-    prices=get_prices(start2,end2,tickers)
-    returns=get_returns(prices)
-    covar=covar_to_np(returns)
-    mean_returns=np_take_mean_of_returns(returns)
-    
-    # Dot returns with weights, get stdev
-    r = weights @ mean_returns
-    s=calc_portfolio_stdev(covar,weights)
-    
-    print('Allocations of optimal portfolio that returns %.2f%% daily from %s to %s:'%(desired_return*100,start1,end1))
-    print_allocations(weights[0],tickers)
-    print('\nThat portfolio performs as follows from %s to %s:'%(start2,end2))
-    print('Return: ' + '{0:.{1}f}'.format((r[0]*100),4) + '%')
-    print('Standard deviation: ' + '{0:.{1}f}'.format((s[0][0]*100),4) + '%')
-    
-    if r[0]>desired_return:
-        print('\nPortfolio had higher returns than the model predicts')
-    elif r[0]<desired_return:
-        print('\nPortfolio had lower returns than the model predicts')
-    else:
-        print('\nThe portfolio performed as predicts')
-    
-    # asks if user wants to test model in another period
-    new = exit_or_cont()
-    if new:
-        backtest(start1,end1,tickers)
-    
-    
+    next_step = backtest_or_restart()
+    if next_step =='backtest':
+        backtest(stds,mean_returns,covar,start1,end1,tickers)
+    elif next_step == 'restart':
+        main()
+        
+def backtest(stds,mean_returns,covar,start1,end1,tickers):
+    next_next_step = simulate_or_reality()
+    if next_next_step == 'simulate':
+        pars = initialize_MCsimulation() # Get simulation parameters
+        r,std,years,n = [float(x) for x in pars] # Cast from string to float
+        sim = MCStockSimulator(years,r,std) # Creates a simulation class
+        sim.plot_simulated_stock_values(int(n)) # Plot simulations
+        sequence(stds,mean_returns,covar,start1,end1,tickers)
+        
+    elif next_next_step == 'real':
+        # Plot curve in other period than used by model
+        realtest_frontier(stds,mean_returns,covar,start1,end1,tickers)
+        sequence(stds,mean_returns,covar,start1,end1,tickers)
