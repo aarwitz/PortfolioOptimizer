7dbeabdeef45acbbfb791dff3e632c669b6bb5e3
Aaron Horwin <61487056+horwina@users.noreply.github.com>
2020-05-23 21:25:38 -0400

    Update user.py

--- user.py
+++ user.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 27 19:37:10 2020
-
-@author: aaronhorwin
+Interact with user
 """
 
-
+import numpy as np
+from prints import *
 
 def get_portfolio_parameters():
     #creates list of tickers, split by spaces
@@ -19,23 +18,45 @@ def get_portfolio_parameters():
 
     return start1,end1,tickers
     
+
     
 def backtest_or_restart():
-    response = input('-Enter restart to change the tickers or the period the model uses data from.\n-Enter backtest to get the actual return of the optimal allocation for any desired return during a different period than the model used (i.e. backtesting).\n\nEnter either backtest or restart: ')
+    response = input('-Enter restart to change the tickers or the period the model uses.\n-Enter backtest to backtest \n\nEnter either backtest or restart: ')
+    return response
+
+
+def simulate_or_reality():
+    print_line()
+    print('\n-Enter real to get the actual return of the optimal allocation for any desired return during a different period than the model used\n-Enter simulate to conduct Monte Carlo simulation')
+    response = input('Enter either real or simulate: ')
+    print_line()
     return response
 
 def backtesting_details(start1,end1):
-    print('\n\nSimply enter your desired return and a range of dates, and the weights of the optimal portfolio from the period %s to %s will be applied to the period entered.'%(start1,end1))
+    print('\n\nSimply enter a range of returns and a range of dates, and the weights of the optimal portfolios from the period %s to %s will be applied to the period entered.'%(start1,end1))
 
-    desired_return=float(input('Enter desired daily return as a %: '))/100
+    quantity_rs = input('Enter how many portfolios to test: ')
+    r1 = input('Enter the minimum return to test (%): ')
+    r2 = input('Enter the maximum return to test (%): ')
+    rs = np.linspace(float(r1)/100,float(r2)/100,int(quantity_rs))
+    
     start2 = input('Enter beginning date for testing (format: year-month-day): ')
     end2 = input('Enter ending date for testing: ')
-    print('_'*100)
-    return desired_return,start2,end2
+    
+    print_line()
+    return rs,start2,end2
+
 
 def exit_or_cont():
-    exit_or_cont = input('Enter new to test this same model during another period or end to end the session: ')
-    if exit_or_cont == 'new':
+    exit_or_cont = input('Enter continue to continue backtesting or end to end the session: ')
+    if exit_or_cont == 'continue':
         return True
     else:
         return False
+    
+def initialize_MCsimulation():
+    r = input('Enter the expected daily return (%): ')
+    std = input('Enter the standard deviation of daily return (%): ')
+    years = input('Enter how many years to simulate: ')
+    n = input('Enter how many trials to run: ')
+    return r,std,years,n
